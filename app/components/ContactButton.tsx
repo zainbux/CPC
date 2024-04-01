41689d30937adc637ded44232ff1a1377bf853d3
zainbux <zainbux99@gmail.com>
2024-04-01 06:10:42 -0400

    Button fix

--- app/components/ContactButton.tsx
+++ app/components/ContactButton.tsx
@@ -7,13 +7,10 @@ const ContactButton = () => {
   const router = useRouter()
 
   return (
-    <button type="button" onClick={() => router.push('mailto:zainbux99@gmail.com')}>
-      Dashboard
+    <button className='bg-indigo-500 text-white py-3 px-5 rounded-full hover:opacity-75 transition hover:cursor-pointer w-36' type="button" onClick={() => router.push('mailto:buxincorporated@gmail.com')}>
+      CONTACT US
     </button>
   );
 };
 
 export default ContactButton;
-
-
-// className='bg-indigo-500 text-white py-3 px-5 rounded-full hover:opacity-75 transition hover:cursor-pointer w-36 '
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { FaMailBulk } from 'react-icons/fa';
 
 type Props = {};
 
 const Footer = (props: Props) => {
+
+  const router = useRouter()
+
   return (
     <div className=' bg-indigo-700 h-[20vh] justify-center flex flex-col items-center gap-3 md:flex-row md:gap-20 py-3 px-5'>
         <div className='flex gap-5'>
@@ -11,11 +17,10 @@ const Footer = (props: Props) => {
         
           <h1 className='md:text-2xl lg:text-4xl font-bold text-white mb-5 md:mb-0'>Interested in working with us? Contact us now</h1>
         </div>
-
-        <div>
-        <div
-          className='bg-slate-200 text-indigo-700 font-bold py-2 px-5 rounded-full hover:opacity-75 transition hover:cursor-pointer w-40 '>CONTACT US</div>
-        </div>
+        
+        <button className='bg-slate-200 text-indigo-700 font-bold py-2 px-5 rounded-full hover:opacity-75 transition hover:cursor-pointer w-40 ' type="button" onClick={() => router.push('mailto:buxincorporated@gmail.com')}>
+          CONTACT US
+        </button>
 
     </div>
   )
